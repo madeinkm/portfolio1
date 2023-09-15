620dab7373d5a9da8a0c81beda09cb7945df43c2
madeinkm <madeinkm@gmail.com>
2023-09-15 18:00:08 +0900

    9월15일 수정

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,18 +1,31 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting.ReorderableList;
 using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    // Start is called before the first frame update
+    [Header("플레이어스텟")]
+    [SerializeField] private float m_Speed;
+    [SerializeField] private int m_MaxHp = 10;
+    [SerializeField] private int m_attack = 1;
+    
     void Start()
     {
         
     }
 
-    // Update is called once per frame
+    
     void Update()
     {
-        
+        moving();
+    }
+
+    private void moving()
+    {
+        float horizontal = Input.GetAxisRaw("Horizontal") * Time.deltaTime;
+        float vertical = Input.GetAxisRaw("Vertical") * Time.deltaTime;
+        transform.position += new Vector3(horizontal, vertical) * m_Speed;
+
     }
 }
