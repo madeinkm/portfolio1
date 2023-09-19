555dd2c24381fd887ef19069314124dd5b6d0eed
madeinkm <madeinkm@gmail.com>
2023-09-19 18:37:29 +0900

    9/19일 오전 수정

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -8,8 +8,11 @@ public class Player : MonoBehaviour
     [Header("플레이어스텟")]
     [SerializeField] private float m_Speed;
     [SerializeField] private int m_MaxHp = 10;
-    [SerializeField] private int m_attack = 1;
-    
+    [SerializeField] private int m_Attack = 1;
+
+    private Transform trsHands;
+
+        
     void Start()
     {
         
@@ -19,13 +22,28 @@ public class Player : MonoBehaviour
     void Update()
     {
         moving();
+        
     }
 
     private void moving()
     {
-        float horizontal = Input.GetAxisRaw("Horizontal") * Time.deltaTime;
-        float vertical = Input.GetAxisRaw("Vertical") * Time.deltaTime;
-        transform.position += new Vector3(horizontal, vertical) * m_Speed;
+        float move_x = Input.GetAxisRaw("Horizontal") * Time.deltaTime; //입력키 설정
+        float move_y = Input.GetAxisRaw("Vertical") * Time.deltaTime;
 
+        transform.position += new Vector3(move_x, move_y) * m_Speed;
+
+        if (move_x > 0) // 입력 방향에 따른 캐릭터 방향설정
+        {
+           transform.localScale = new Vector3(1.0f, 1.0f, 1.0f);
+        }
+        else if (move_x < 0)
+        {
+            transform.localScale = new Vector3(-1.0f, 1.0f, 1.0f);
+        }
+    }
+
+    private void attack()
+    {
+        
     }
 }
